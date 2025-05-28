300157fa7b12ff9e6c030f85cfc643226f7769ec
Tudor <tudor204@hotmail.com>
2025-05-28 11:23:12 +0200

    templates en flask y ejercicio de calculadora

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #importar la libería de flask
-from flask import Flask
+from flask import Flask, render_template
 
 #inicializar la variable ap con flask
 app = Flask(__name__)
@@ -9,9 +9,9 @@ app = Flask(__name__)
 #en windows: set FLASK_APP=main.py
 
 #comando para ejecutar el servidor
-#flask --app main
+#flask --app main run
 
-#Comando para ejecutar el servidor en otro puerto diferente por default es el 5000
+#Comando para ejecutar el servidor en otro puerto diferente por defecto es el 5000
 #flask --app main run -p 5002
 
 #Comando para ejeutar el servidor en modo debug, para realizar cambios en tiempo real 
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 @app.route("/hola")
 def hello_world():
-    return "Hello, World, mundo flask!"
+    return "Hola mundo, mundo flask!"
 
 #ejercicio una ruta que devualva una lista de frutas el path sería /frutas
 @app.route("/frutas")
@@ -28,9 +28,9 @@ def lista_frutas():
     return list_fruta
 
 #ejemplo para enviar parametros en las rutas
-@app.route("/nombre/<n>/")
-def tunombre(n):
-    return f"hola {n} como estás"
+@app.route("/nombre/<n>/apellido<a>/edad<int:e>")
+def tunombre(n,a,e):
+    return f"hola {n} {a} tienes {e} años de edad"
 
 #ejercicio2 realizar una ruta que devualva el cuadrado de un numero dado,/numero/<parametro>
 @app.route("/numero/<int:parametro>")
@@ -39,4 +39,19 @@ def cuadrado(parametro):
     return f"El cuadrado de {parametro} es {parametro*parametro}"
 
 #ejercicio3, realizar una ruta, que dinamicamente pueda soliciar o realizar 
-#operaciones de suma, resta, multiplicación y division segun los parametros pasods en la ruta
+#operaciones de suma, resta, multiplicación y division segun los parametros pasados en la ruta
+@app.route("/operaciones/<float:op1>/<float:op2>/<string:ope>")
+def calcualdora(op1,op2,ope):
+    if ope == "sum":
+        return f"La suma de {op1} y  {op2} es {op1+op2}"
+    elif ope == "rest":
+        return f" La resta de {op1} y {op2} es {op1-op2}"
+    elif ope == "mult":
+        return f"La multiplicación de {op1} y {op2} es {op1*op2}"
+    elif ope == "div":
+        return f"La división de {op1} y {op2} es {op1/op2}"
+    
+@app.route("/<nombre>")
+def llamarhtml(nombre):
+    list_fruta = ["Platano","Melon","Naranja","Melocoton","Fresa","Piña"]
+    return render_template("hola.html",name=nombre, fruta= list_fruta)
